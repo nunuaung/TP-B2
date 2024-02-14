27b6d9a91b6cc52932a2f1bd99534897e343294f
Nu Nu Aung <nunuaung942@gmail.com>
2024-02-14 16:12:47 +0630

    add axios

--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const About = () => {
-
+const [dataList,setDataList] = useState([]);
   // GET
   // useEffect(()=>{
   //   fetch('https://jsonplaceholder.typicode.com/posts')
@@ -11,25 +12,46 @@ const About = () => {
   // },[])
 
   //POST
-  const paramStrings = {
-    method: 'POST',
-    header: 'Content-Type, application/json',
-    body: JSON.stringify({
-      body: "new data",
-      id: 101,
-      title: "new title",
-      userId: "user1",
-    })
+  // const paramStrings = {
+  //   method: 'POST',
+  //   header: 'Content-Type, application/json',
+  //   body: JSON.stringify({
+  //     body: "new data",
+  //     id: 101,
+  //     title: "new title",
+  //     userId: "user1",
+  //   })
+  // }
+  // useEffect(()=>{
+  //   fetch('https://jsonplaceholder.typicode.com/posts',paramStrings)
+  //   .then((res)=>res.json())
+  //   .then((data)=>console.log(`${data.id} has been created`))
+  // },[])
+
+  const body = {
+    body: 'new data 1',
+    id: 101,
+    title: 'new title 1',
+    userId: 'user2',
   }
+
   useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/posts',paramStrings)
-    .then((res)=>res.json())
-    .then((data)=>console.log(`${data.id} has been created`))
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>setDataList(res.data))
+  },[])
+
+  useEffect(()=>{
+    axios.post('https://jsonplaceholder.typicode.com/posts',body).then((res)=>console.log(res.data.id))
   },[])
 
 
   return (
-    <div>About</div>
+    <div>About
+      {
+        dataList.map((item)=>
+        <p key={item.id}>{item.title}</p>
+        )
+      }
+    </div>
   )
 }
 
